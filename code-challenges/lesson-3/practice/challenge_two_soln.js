92796723278246e2efe6868aee809cfb4d1a0f36
Loretta Owusu-Ansah <lorettaowusuansah6@gmail.com>
2023-11-30 15:15:12 +0000

    Completed challenge two

---
+++ code-challenges/lesson-3/practice/challenge_two_soln.js
@@ -0,0 +1,18 @@
+//Question 
+/*There are three things wrong with this code.Find them and exoplain why they are wrong.
+
+const some Number =20;
+someNumber=50
+
+*/
+
+//Answer 
+/*Issue 1
+The name of the variable is incorrect .It should be someNumber instaed of some Number.
+
+Issue 2 
+const cannot be used when we are trying to re-assign a variable.Instead, (let) is rather used.
+
+Issue 3
+The statement was not closed with a semi-colon at the end.
+*/
