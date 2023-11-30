290e35038fa484770af94f7dfaa2b10f19a1edba
Loretta Owusu-Ansah <lorettaowusuansah6@gmail.com>
2023-11-30 15:56:19 +0000

    Completed challenge 3,4 and 5 of lesson 3 practices

---
+++ code-challenges/lesson-3/practice/challenge_five_soln.js
@@ -0,0 +1,12 @@
+/*
+Why does this code not work?Edit it until it works 
+ */
+//Code here
+const myArray = [20, 30, 40];
+
+console.log(myArray[3]);
+
+//This code is valid but the console.log requirements are inavlid because the variable myArray has only 3 instaead of 4 indexes sated above so it's supposed to be console.log(myArray[2]) since it counts from index 0 so the outputnis 40.
+
+//Corrected console.log
+console.log(myArray[2]);
